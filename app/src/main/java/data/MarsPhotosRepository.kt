58a5b42650a646e0f4d11d11b941e865d2ff9153
GeraGarcia10 <s22120009@alumnos.itsur.edu.mx>
2025-01-30 22:22:32 -0600

    Se coloca la clase NetworkMarsPhotosRepository, con el cual se va a anular la función abstracta getMarsPhotos()

--- app/src/main/java/data/MarsPhotosRepository.kt
+++ app/src/main/java/data/MarsPhotosRepository.kt
@@ -6,3 +6,9 @@ import com.example.marsphotos.network.MarsApiService
 interface MarsPhotosRepository {
     suspend fun getMarsPhotos(): List<MarsPhoto>
 }
+
+class NetworkMarsPhotosRepository(
+    private val marsApiService: MarsApiService
+) : MarsPhotosRepository {
+    override suspend fun getMarsPhotos(): List<MarsPhoto> = marsApiService.getPhotos()
+}
